1dadf646636082e55459a224029d9c05d3ec62ea
Trecker <treckerformal@gmail.com>
2026-08-27 19:10:28 -0600

    [fix]Cloudflare

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,19 +26,15 @@ export default function RootLayout({
 }>) {
 
   return (
-    <>
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <path>
-      {children}
-        </path>
-        </body>
-      
+        <AppShell>
+          {children}
+        </AppShell>
+      </body>
     </html>
-    </>
- 
   );
 }
